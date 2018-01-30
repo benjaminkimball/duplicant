f449bb12f9e77451caad531b288e8e2d2cee5130
Benjamin Kimball <benjamin.r.kimball@gmail.com>
2018-01-30 10:27:53 -0700

    Change casing on chunk names

--- template/client/chunks.js
+++ template/client/chunks.js
@@ -1,5 +1,5 @@
 import asyncComponent from './components/async-component'
 
 export const NotFoundPage = asyncComponent(() => (
-  import(/* webpackChunkName: "NotFoundPage" */ './components/not-found-page')
+  import(/* webpackChunkName: "not-found-page" */ './components/not-found-page')
 ))
