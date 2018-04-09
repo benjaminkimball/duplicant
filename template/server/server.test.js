6b997ef8f20ec1e8b2a7417e7fe12a9469bb5554
Benjamin Kimball <benjamin.r.kimball@gmail.com>
2018-04-09 09:01:13 -0600

    Add basic test example using mocha and chai

---
+++ template/server/server.test.js
@@ -0,0 +1,10 @@
+import { expect } from 'chai'
+import { describe, it } from 'mocha'
+
+import server from './server'
+
+describe('The server', () => {
+  it('smells like an Express app', () => {
+    expect(typeof server.use).to.equal('function')
+  })
+})
